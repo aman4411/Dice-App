1e76198e0a6a80f54f12aaa7f56a6daaf7799541
aman4411 <amanmehta4411@gmail.com>
2020-04-12 00:03:39 +0530

    dice functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,70 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                  onPressed: () {
+                    changeDice();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              leftDiceNumber = 1;
+              rightDiceNumber = 1;
+            });
+          },
+          child: Text(
+            'Reset',
+            style: TextStyle(
+                color: Colors.red, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+        ),
+      ],
+    );
   }
 }
